8c5e99782537fad63417ff1ad0ffa67867c22a0c
AmanMasipeddi <aman.masipeddi@gmail.com>
2020-01-30 06:20:49 -0600

    commited

--- lib/Location_Pointer_Map_Page.dart
+++ lib/Location_Pointer_Map_Page.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
 import 'package:location/location.dart';
+import 'static_map_provider.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LocationPointerMapPage extends StatefulWidget {
 
-   Position currentPosition;
+  Position currentPosition;
   LocationPointerMapPage(this.currentPosition);
 
   @override
@@ -20,9 +21,11 @@ class _LocationPointerMapPageState extends State<LocationPointerMapPage>{
   Position currentPosition;
   _LocationPointerMapPageState(this.currentPosition);
 
-
   Location _location = new Location();
+  List locations = [];
+  String googleMapsApi = 'AIzaSyBpPIFHijTtZM2glN2WZrpXfooIGzpkz9A';
   dynamic deviceLocation;
+  int zoom =5;
 
   @override
   void initState(){
@@ -45,14 +48,16 @@ class _LocationPointerMapPageState extends State<LocationPointerMapPage>{
                   decoration: BoxDecoration(
                     //border: Border.all(width: 2),
                   ),
+                  child: new StaticMapsProvider(googleMapsApi,currentPosition, zoom),
                 )
             ),
             Expanded(
-                child: Container(
+                child: Container(margin: EdgeInsets.all(10),
                   decoration: BoxDecoration(
                     //border: Border.all(width: 2),
                   ),
-                  child: Text("${currentPosition.latitude} & ${currentPosition.longitude} & $_currentAddress"),
+                  child:
+                  Text("$_currentAddress", style: TextStyle(fontSize: 20)),
                 )
             )
           ],
@@ -81,5 +86,6 @@ class _LocationPointerMapPageState extends State<LocationPointerMapPage>{
   }
 
 }
+
 
 
